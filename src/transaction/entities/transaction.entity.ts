709b1b306332f41f1708299c90a9491ddc892381
SrAndrii <55akif5548@gmail.com>
2025-02-01 15:48:54 -0800

    feat(db): implemented initial database schema with entities

---
+++ src/transaction/entities/transaction.entity.ts
@@ -0,0 +1,32 @@
+import { Category } from "src/category/entities/category.entity";
+import { User } from "src/user/entities/user.entity";
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class Transaction {
+    @PrimaryColumn({name: 'transaction_id'})
+    id:number
+
+    @Column()
+    title:string
+
+    @Column({nullable:true})
+    type:string
+
+    @Column()
+    amount:number
+    
+    @ManyToOne(()=>User, (user)=>user.transactions)
+    @JoinColumn({name: 'user_id'})
+    user:User
+
+    @ManyToOne(()=>Category, (category)=>category.transactions)
+    @JoinColumn({name: 'category_id'})
+    category:Category[]
+
+    @CreateDateColumn()
+    createdAt:Date
+    
+    @UpdateDateColumn()
+    updatedAt:Date
+}
